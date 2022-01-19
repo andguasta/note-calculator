423549d1ed3efdf25fee16a98ac7e4cf49d147e6
andrea <andguasta@gmail.com>
2022-01-19 13:56:33 +0100

    added command line arguments

--- notecalc.c
+++ notecalc.c
@@ -16,89 +16,119 @@ int main(int argc, char const *argv[])
     char* userInput;
     
     char noteInput[4];
-    //Using this only for testing
-    if (argc > 1)
-    {        
-        return 0;
-    }
-      
-    userInput = malloc(sizeof(char)*100);
-    while(1)
+
+    if (argc == 1) // Prompt User
     {
-        printf("1 to input note name; 2 to input frequency\n");
-        scanf("%s", userInput);
-        if (strlen(userInput) > 1)
-        {
-            printf("Invalid input\n");
-        }
-        else
-        {
-            mode = (int)(userInput[0]-'0');
-            if (mode == 0 || mode > NUM_MODES)
-            {
-                printf("Invalid mode number\n");
-            }
-            else
-            {
-                break;
-            }                        
-        }
-    }        
-    if (mode == 1)
-    {
+        userInput = malloc(sizeof(char)*100);
         while(1)
         {
-            printf("Enter Note plus octave Ex. C4 or Eb3: ");
+            printf("1 to input note name; 2 to input frequency\n");
             scanf("%s", userInput);
-            if (strlen(userInput) > 3)
+            if (strlen(userInput) > 1)
             {
-                printf("Please enter a valid note Ex. C4 or Eb3:\n");
+                printf("Invalid input\n");
             }
             else
             {
-                strncpy(noteInput, userInput, 4);
-                int midiNote = string2midi(noteInput);
-                if (midiNote == -1)
+                mode = (int)(userInput[0]-'0');
+                if (mode == 0 || mode > NUM_MODES)
                 {
-                    printf("The note is in an invalid format\n");
+                    printf("Invalid mode number\n");
                 }
                 else
                 {
                     break;
                 }                        
-            }        
-        }
-        noteFrequency = midi2Freq(string2midi(noteInput));
-        printf("The Note Frequency is %0.2fhz\n", noteFrequency);
-        printf("The Note Midi Number is: %d\n", string2midi(noteInput));
-    }
-    else if (mode == 2)
-    {
-        double userFrequency;
-        while(1)
-        {
-            //short validity = 0;
-            printf("Enter a frequency Ex 350:\n");
-            scanf("%s", userInput);
-            char* garbageInput;
-            userFrequency = strtod(userInput, &garbageInput);
-            if (userFrequency > 0.0f)
-            {
-                break;
             }
-            else
+        }        
+        if (mode == 1)
+        {
+            while(1)
             {
-                printf("Please enter a valid Input\n");
-            }         
+                printf("Enter Note plus octave Ex. C4 or Eb3: ");
+                scanf("%s", userInput);
+                if (strlen(userInput) > 3)
+                {
+                    printf("Please enter a valid note Ex. C4 or Eb3:\n");
+                }
+                else
+                {
+                    strncpy(noteInput, userInput, 4);
+                    int midiNote = string2midi(noteInput);
+                    if (midiNote == -1)
+                    {
+                        printf("The note is in an invalid format\n");
+                    }
+                    else
+                    {
+                        break;
+                    }                        
+                }        
+            }
+            noteFrequency = midi2Freq(string2midi(noteInput));
+            printf("The Note Frequency is %0.2fhz\n", noteFrequency);
+            printf("The Note Midi Number is: %d\n", string2midi(noteInput));
         }
-        // double userFrequency = atoi(userInput);
-        int noteNumber = -1;
-        noteNumber = freq2Midi(userFrequency);
-        printf("The Note Midi Number is: %d\n", noteNumber);
-        char noteName[4];
-        midi2String(noteNumber, noteName);
-        printf("The Note Name is: %s\n", noteName);
-    }\
-    free(userInput);
+        else if (mode == 2)
+        {
+            double userFrequency;
+            while(1)
+            {
+                //short validity = 0;
+                printf("Enter a frequency Ex 350:\n");
+                scanf("%s", userInput);
+                char* garbageInput;
+                userFrequency = strtod(userInput, &garbageInput);
+                if (userFrequency > 0.0f)
+                {
+                    break;
+                }
+                else
+                {
+                    printf("Please enter a valid Input\n");
+                }         
+            }
+            // double userFrequency = atoi(userInput);
+            int noteNumber = -1;
+            noteNumber = freq2Midi(userFrequency);
+            printf("The Note Midi Number is: %d\n", noteNumber);
+            char noteName[4];
+            midi2String(noteNumber, noteName);
+            printf("The Note Name is: %s\n", noteName);
+        }
+        free(userInput);
+    }
+    else
+    {
+        char* tempPoint; 
+        if (strncmp(argv[1],"-m", 2) == 0)
+        {            
+            short midiNum;
+            midiNum = (short)strtod(argv[2], &tempPoint);
+            printf("Midi Note %d -> Freq: %0.2fhz\n", midiNum, midi2Freq(midiNum));
+        }
+        else if (strncmp(argv[1],"-f", 2) == 0)
+        {         
+            double frequency;
+            frequency = (short)strtod(argv[2], &tempPoint);
+            printf("Freq: %0.2fhz -> Midi Note %d\n", frequency, freq2Midi(frequency));
+        }
+        else if (strncmp(argv[1],"-s", 2) == 0)
+        {
+            strncpy(noteInput, argv[2], 3);
+            noteInput[3] = '\0';            
+            printf("Note %s -> Midi Note %d -> Freq: %0.2fhz\n", noteInput, string2midi(noteInput), midi2Freq(string2midi(noteInput)));
+        }
+        else if(strncmp(argv[1],"-7", 2) == 0)
+        {
+            //Using this only for testing
+        }
+        else
+        {
+            printf("Argument non valid\n");
+        }
+        
+    }
+      
     return 0;
 }
